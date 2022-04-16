1e62dc9aa5d5b88599f094b5dde115e36e94f400
Ersad Ishlahuddin <ersadishlahuddin@gmail.com>
2022-04-16 14:46:56 +0700

    Create two-sum_test.go

---
+++ leetcode/problem/two-sum_test.go
@@ -0,0 +1,51 @@
+package problem_test
+
+import (
+	"1d1c/leetcode/problem"
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "2 7 11 15 target 9 want 0 1",
+			args: args{
+				nums:   []int{2, 7, 11, 15},
+				target: 9,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "3 2 4 target 6 want 1 2",
+			args: args{
+				nums:   []int{3, 2, 4},
+				target: 6,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "3 3 target 6 want 0 1",
+			args: args{
+				nums:   []int{3, 3},
+				target: 6,
+			},
+			want: []int{0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := problem.TwoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TwoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
